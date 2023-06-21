aa630de2c16041760b0fd5f71a7a1eabe65b60b0
Ahmad Arif Saputra <ahmadarifsaputra@gmail.com>
2023-06-21 13:37:08 +0700

    [Warga] : Add Modal for Add Data Warga

--- src/container/pages/DaftarWarga/index.js
+++ src/container/pages/DaftarWarga/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useEffect, useState } from "react";
 // Style
 import "./index.scss";
@@ -9,7 +11,21 @@ import { HeadingDashboard } from "../../../components/atoms";
 import axios from "axios";
 import Moment from "react-moment";
 import { v1 as uuidv1 } from "uuid";
-import { Table, Button, Popconfirm, Layout, Typography, Breadcrumb, Divider, Input, Modal, Form, Radio, Select } from "antd";
+import {
+  Table,
+  Button,
+  Popconfirm,
+  Layout,
+  Typography,
+  Breadcrumb,
+  Divider,
+  Input,
+  InputNumber,
+  Modal,
+  Form,
+  Radio,
+  Select,
+} from "antd";
 const { Text } = Typography;
 const { Option } = Select;
 const formItemLayout = {
@@ -31,7 +47,10 @@ const DaftarWarga = () => {
   // State Search
   const [searchText, setSearchText] = useState("");
   // show/hide Modal
-  const [visible, setVisible] = useState(false);
+  const [visibleModalAdd, setVisibleModalAdd] = useState(false);
+  const [visibleModalEdit, setVisibleModalEdit] = useState(false);
+  // State getKey
+  const [keyData, setKeyData] = useState(null);
 
   //useEffect get Data List Table
   useEffect(() => {
@@ -60,66 +79,68 @@ const DaftarWarga = () => {
       title: "Nama Warga",
       dataIndex: "namaWarga",
       key: "namaWarga",
-      width: "30%",
+      width: "13%",
+      // fixed: 'left',
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Alamat",
       dataIndex: "alamat",
       key: "alamat",
-      width: "30%",
+      width: "20%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Nama Perumahan",
       dataIndex: "namaPerumahan",
       key: "namaPerumahan",
-      width: "30%",
+      width: "13%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Email",
       dataIndex: "email",
       key: "email",
-      width: "30%",
+      width: "15%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Jenis Kelamin",
       dataIndex: "jenisKelamin",
       key: "jenisKelamin",
-      width: "30%",
+      width: "10%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Nomor Handphone",
       dataIndex: "noHandphone",
       key: "noHandphone",
-      width: "30%",
+      width: "10%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Status Pernikahan",
       dataIndex: "statusPernikahan",
       key: "statusPernikahan",
-      width: "30%",
+      width: "10%",
       render: (text) => <Text>{text}</Text>,
     },
     {
       title: "Tanggal Bergabung",
       dataIndex: "joinDate",
       key: "joinDate",
-      width: "30%",
+      width: "10%",
       render: (text) => <Moment format="DD MMMM YYYY">{text}</Moment>,
     },
     {
       title: "Actions",
       dataIndex: "actions",
       key: "actions",
+      // fixed: 'right',
+      width: "10%",
       render: (_, record) => (
         <Layout className="table-actions">
-          <Button onClick={() => handleAdd(record)}>Add</Button>
-          <Button onClick={() => handleEdit(record)}>Edit</Button>
+          <Button onClick={() => showModalEdit(record)}>Edit</Button>
           <Popconfirm title="Are you sure?" onConfirm={() => handleDelete(record)}>
             <Button>Delete</Button>
           </Popconfirm>
@@ -142,24 +163,25 @@ const DaftarWarga = () => {
       )
     : [];
   // Modal (Show & Hide)
-  const showModal = () => {
-    setVisible(true);
+  const showModalAdd = () => {
+    setVisibleModalAdd(true);
+  };
+  const showModalEdit = (record) => {
+    console.log(record,'data record')
+    console.log(data[record.key],'data record key')
+    setKeyData(record.key);
+    setVisibleModalEdit(true);
   };
   const handleCancel = () => {
     console.log("test");
-    setVisible(false);
-  };
-  const handleFormSubmit = (values) => {
-    console.log(values, "value"); // Do something with the form values
-    setVisible(false); // Close the modal after form submission
+    setVisibleModalAdd(false);
+    setVisibleModalEdit(false);
   };
   // Handle Add Data Warga
-  const handleAdd = (record) => {
-    console.log(data[record.key], "cek data");
+  const handleFormSubmitAdd = (values) => {
+    console.log(values, "value"); // Do something with the form values
     setLoadData(false);
-    addListWarga(data[record.key]);
-    // Handle edit action
-    console.log("Edit:", record);
+    addListWarga(values);
   };
   const addListWarga = (data) => {
     console.log(data, "proses tambah data");
@@ -169,18 +191,18 @@ const DaftarWarga = () => {
 
     const payload = {
       id_warga: uuidv1(),
-      nama_warga: "mimin",
+      nama_warga: data.name,
       id_perumahan: localStorage.getItem("id_perumahan"),
-      blok_rumah: "X",
-      nomor_rumah: "21",
-      email: "amin@gmail.com",
-      nomor_hp: "083290766622",
-      is_rt: false,
-      is_rw: true,
-      id_rt: "2",
-      id_rw: "1",
-      jenis_kelamin: "Laki-Laki",
-      status_pernikahan: "Menikah",
+      blok_rumah: data.blokRumah,
+      nomor_rumah: data.nomorRumah,
+      email: data.email,
+      nomor_hp: data.nomorHp,
+      is_rt: data.pengurusRt,
+      is_rw: data.pengurusRw,
+      id_rt: `${data.rt}`,
+      id_rw: `${data.rw}`,
+      jenis_kelamin: data.jenisKelamin,
+      status_pernikahan: data.statusPernikahan,
     };
 
     axios
@@ -189,6 +211,7 @@ const DaftarWarga = () => {
         if (response.status === 200) {
           console.log(response, "response Add");
           setLoadData(true);
+          setVisibleModalAdd(false); // Close the modal after form submission
         }
       })
       .catch((error) => {
@@ -196,12 +219,10 @@ const DaftarWarga = () => {
       });
   };
   // Handle Edit Data Warga
-  const handleEdit = (record) => {
-    console.log(data[record.key], "cek data");
+  const handleFormSubmitEdit = (values) => {
+    console.log(values, "value"); // Do something with the form values
     setLoadData(false);
-    updateListWarga(data[record.key]);
-    // Handle edit action
-    console.log("Edit:", record);
+    updateListWarga(values);
   };
   const updateListWarga = (data) => {
     console.log(data, "ini cek data update");
@@ -309,66 +330,68 @@ const DaftarWarga = () => {
           content="Informasi lengkap tentang penduduk atau warga suatu wilayah yang dikumpulkan dan disimpan. Ini mencakup identitas, demografi, kesehatan, dan kegiatan warga untuk membantu pengelolaan data dan pelayanan publik yang efisien. Aplikasi ini memastikan kepatuhan terhadap kebijakan privasi dan perlindungan data, sehingga memberikan gambaran komprehensif mengenai warga dalam satu platform yang terintegrasi."
         />
         <Layout className="dashboard-kita-warga-layout-detail-data-user-content">
-          <Button
-            className="dashboard-kita-warga-layout-detail-data-user-content-button-add"
-            type="primary"
-            icon={<PlusOutlined />}
-            onClick={showModal}
-          >
-            Tambah Warga
-          </Button>
-          <input
-            type="text"
-            placeholder="Search by name..."
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
+          <Layout className="dashboard-kita-warga-layout-detail-data-user-content-header">
+            <Button
+              className="dashboard-kita-warga-layout-detail-data-user-content-header-button-add"
+              type="primary"
+              icon={<PlusOutlined />}
+              onClick={showModalAdd}
+            >
+              Tambah Warga
+            </Button>
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </Layout>
           <Table
             className="dashboard-kita-warga-layout-detail-data-user-content-table"
             columns={wargaColumns}
             dataSource={filteredData}
             loading={loading}
             scroll={{
-              x: 1300,
+              x: 2000,
             }}
           />
         </Layout>
       </Layout>
       <Modal
-        title="My Modal"
+        title="Tambah Data Warga"
         centered
-        open={visible}
+        open={visibleModalAdd}
         onCancel={handleCancel}
         closable={true} // Disable the close button in the header
         maskClosable={false} // Disable closing the modal by clicking outside
         destroyOnClose // Optional: Destroy the modal when it is closed
         footer={null}
       >
-        <Form {...formItemLayout} onFinish={handleFormSubmit}>
-          <Form.Item 
-            name="name" 
-            label="Name" 
+        <Form {...formItemLayout} onFinish={handleFormSubmitAdd}>
+          <Form.Item
+            name="name"
+            label="Name"
             rules={[
-              { 
-                required: true, 
-                message: "Please enter your Name" 
-              }
+              {
+                required: true,
+                message: "Please enter your Name",
+              },
             ]}
           >
             <Input />
           </Form.Item>
-          <Form.Item 
-            name="email" 
-            label="Email" 
+          <Form.Item
+            name="email"
+            label="Email"
             rules={[
               {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
+                type: "email",
+                message: "The input is not valid E-mail!",
               },
-              { 
-                required: true, 
-                message: "Please enter your Email" 
-              }
+              {
+                required: true,
+                message: "Please enter your Email",
+              },
             ]}
           >
             <Input />
@@ -391,7 +414,7 @@ const DaftarWarga = () => {
             ]}
           >
             <Radio.Group>
-              <Radio.Button value="Laki-Laki">Laki - Laki</Radio.Button>
+              <Radio.Button value="Laki - Laki">Laki - Laki</Radio.Button>
               <Radio.Button value="Perempuan">Perempuan</Radio.Button>
             </Radio.Group>
           </Form.Item>
@@ -427,40 +450,190 @@ const DaftarWarga = () => {
           >
             <Input />
           </Form.Item>
-          <Form.Item name="rt" label="RT" rules={[{ required: true, message: "Please enter your name" }]}>
-            <Input />
+          <Form.Item
+            name="rt"
+            label="RT"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <InputNumber />
           </Form.Item>
-          <Form.Item name="rw" label="RW" rules={[{ required: true, message: "Please enter your name" }]}>
-            <Input />
+          <Form.Item
+            name="rw"
+            label="RW"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <InputNumber />
           </Form.Item>
           <Form.Item
             name="pengurusRt"
             label="Pengurus RT"
             rules={[{ required: true, message: "Please enter your name" }]}
           >
-            <Input />
+            <Radio.Group>
+              <Radio.Button value={true}>Iya</Radio.Button>
+              <Radio.Button value={false}>Tidak</Radio.Button>
+            </Radio.Group>
           </Form.Item>
           <Form.Item
             name="pengurusRw"
             label="Pengurus RW"
             rules={[{ required: true, message: "Please enter your name" }]}
           >
-            <Input />
+            <Radio.Group>
+              <Radio.Button value={true}>Iya</Radio.Button>
+              <Radio.Button value={false}>Tidak</Radio.Button>
+            </Radio.Group>
           </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        title="Ubah Data Warga"
+        centered
+        open={visibleModalEdit}
+        onCancel={handleCancel}
+        closable={true} // Disable the close button in the header
+        maskClosable={false} // Disable closing the modal by clicking outside
+        destroyOnClose // Optional: Destroy the modal when it is closed
+        footer={null}
+      >
+        <Form
+          {...formItemLayout}
+          onFinish={handleFormSubmitEdit}
+          initialValues={{
+            name: `${data && data[keyData] ? data[keyData].nama_warga : ""}`,
+            email: `${data && data[keyData] ? data[keyData].email : ""}`,
+            nomorHp: `${data && data[keyData] ? data[keyData].nomor_hp : ""}`,
+            jenisKelamin: `${data && data[keyData] ? data[keyData].jenis_kelamin : ""}`,
+            statusPernikahan: `${data && data[keyData] ? data[keyData].status_pernikahan : ""}`,
+            blokRumah: `${data && data[keyData] ? data[keyData].blok_rumah : ""}`,
+            nomorRumah: `${data && data[keyData] ? data[keyData].nomor_rumah : ""}`,
+            rt: `${data && data[keyData] ? data[keyData].id_rt : ""}`,
+            rw: `${data && data[keyData] ? data[keyData].id_rw : ""}`,
+            pengurusRt: data && data[keyData] ? data[keyData].is_rt : "",
+            pengurusRw: data && data[keyData] ? data[keyData].is_rw : "",
+
+
+          }}
+        >
           <Form.Item
-            name="radio-button"
-            label="Radio.Button"
+            name="name"
+            label="Name"
             rules={[
               {
                 required: true,
-                message: "Please pick an item!",
+                message: "Please enter your Name",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please enter your Email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="nomorHp"
+            label="Nomor HP"
+            rules={[{ required: true, message: "Please enter your Nomor HP" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="jenisKelamin"
+            label="Jenis Kelamin"
+            rules={[
+              {
+                required: true,
+                message: "Please pick an Jenis Kelamin",
               },
             ]}
           >
             <Radio.Group>
-              <Radio.Button value={true}>item 1</Radio.Button>
-              <Radio.Button value={false}>item 2</Radio.Button>
-              <Radio.Button value="c">item 3</Radio.Button>
+              <Radio.Button value="Laki - Laki">Laki - Laki</Radio.Button>
+              <Radio.Button value="Perempuan">Perempuan</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            name="statusPernikahan"
+            label="Status Pernikahan"
+            // hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please select your country!",
+              },
+            ]}
+          >
+            <Select placeholder="Pilih Status Pernikahan">
+              <Option value="Belum Kawin">Belum Kawin</Option>
+              <Option value="Kawin">Kawin</Option>
+              <Option value="Cerai Hidup">Cerai Hidup</Option>
+              <Option value="Cerai Mati">Cerai Mati</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="blokRumah"
+            label="Blok Rumah"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="nomorRumah"
+            label="Nomor Rumah"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="rt"
+            label="RT"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item
+            name="rw"
+            label="RW"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <InputNumber />
+          </Form.Item>
+          <Form.Item
+            name="pengurusRt"
+            label="Pengurus RT"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <Radio.Group>
+              <Radio.Button value={true}>Iya</Radio.Button>
+              <Radio.Button value={false}>Tidak</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            name="pengurusRw"
+            label="Pengurus RW"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <Radio.Group>
+              <Radio.Button value={true}>Iya</Radio.Button>
+              <Radio.Button value={false}>Tidak</Radio.Button>
             </Radio.Group>
           </Form.Item>
           <Form.Item>
